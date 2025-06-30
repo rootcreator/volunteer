e90316f1c3ccd52928f48374af7eb9db85437278
Rootcreator <maarrxxy@gmail.com>
2025-06-30 07:06:13 +0100

    V0.2.1-untested

--- lib/main.dart
+++ lib/main.dart
@@ -10,27 +10,42 @@ import 'screens/volunteer/volunteer_analytics_screen.dart';
 import 'screens/volunteer/volunteer_event_detail_screen.dart';
 import 'screens/volunteer/volunteer_events_screen.dart';
 import 'services/notification_service.dart';
-
 import 'firebase_options.dart';
-
-
-
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await NotificationService.init();
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  await messaging.requestPermission();
+
+  String? token = await messaging.getToken();
+  print("FCM Token: $token");
 
   FirebaseMessaging.onMessage.listen((RemoteMessage message) {
     NotificationService.showLocalNotification(message);
   });
 
+  // Optional: handle background/terminated notifications
+  setupFCMHandlers();
+
   runApp(const VolunteerApp());
 }
+
+void setupFCMHandlers() {
+  FirebaseMessaging.onMessageOpenedApp.listen((message) {
+    print("Notification opened in background");
+  });
+
+  FirebaseMessaging.instance.getInitialMessage().then((message) {
+    if (message != null) {
+      print("Notification opened from terminated");
+    }
+  });
+}
+
 
 class VolunteerApp extends StatelessWidget {
   const VolunteerApp({super.key});
@@ -43,17 +58,17 @@ class VolunteerApp extends StatelessWidget {
       home: const AuthGate(),
 
       // Optional: add named routes if you use them
-      routes: {
-        '/login': (_) => const AuthGate(),
-        '/home': (_) => const HomeScreen(),
-        '/admin events': (_) => const AdminEventListScreen(),
-        '/add event': (_) => const CreateEventScreen(),
-        '/calendar': (_) => const CalendarScreen(),
-        '/event details': (_) => const VolunteerEventDetailScreen(eventId: '',),
-        '/events for volunteers': (_) => const VolunteerEventsScreen(),
-        '/analytics': (_) => const VolunteerAnalyticsScreen()
+        routes: {
+          '/login': (_) => const AuthGate(),
+          '/home': (_) => const HomeScreen(),
+          '/adminEvents': (_) => const AdminEventListScreen(),
+          '/addEvent': (_) => const CreateEventScreen(),
+          '/calendar': (_) => const CalendarScreen(),
+          '/eventDetails': (_) => const VolunteerEventDetailScreen(eventId: ''),
+          '/volunteerEvents': (_) => const VolunteerEventsScreen(),
+          '/analytics': (_) => const VolunteerAnalyticsScreen()
+        }
 
-      },
     );
   }
 }
